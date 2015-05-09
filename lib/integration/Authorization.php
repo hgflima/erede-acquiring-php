af40e09707cef13d6daafac0faeb203caf105f88
Henrique Lima <hgflima@gmail.com>
2015-05-09 11:19:17 -0300

    cleaning integration/Authorization class

--- lib/integration/Authorization.php
+++ lib/integration/Authorization.php
@@ -2,75 +2,17 @@
 
 namespace ERede\Acquiring\Integration;
 
-class Authorization
-{
+class Authorization {
 
-    /**
-     * @var string $CodRet
-     * @access public
-     */
     public $CodRet = null;
-
-    /**
-     * @var string $Data
-     * @access public
-     */
     public $Data = null;
-
-    /**
-     * @var string $MsgAvs
-     * @access public
-     */
     public $MsgAvs = null;
-
-    /**
-     * @var string $Msgret
-     * @access public
-     */
     public $Msgret = null;
-
-    /**
-     * @var string $NumAutent
-     * @access public
-     */
     public $NumAutent = null;
-
-    /**
-     * @var string $NumAutor
-     * @access public
-     */
     public $NumAutor = null;
-
-    /**
-     * @var string $NumPedido
-     * @access public
-     */
     public $NumPedido = null;
-
-    /**
-     * @var string $NumSqn
-     * @access public
-     */
     public $NumSqn = null;
-
-    /**
-     * @var string $RespAvs
-     * @access public
-     */
     public $RespAvs = null;
-
-    /**
-     * @var string $Tid
-     * @access public
-     */
     public $Tid = null;
-
-    /**
-     * @access public
-     */
-    public function __construct()
-    {
-    
-    }
 
 }
