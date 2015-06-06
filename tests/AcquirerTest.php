bb05d0c95878a0263a92cab3ec99959ee3dcd5d6
Henrique Lima <hgflima@gmail.com>
2015-06-05 22:41:29 -0300

    acquirer test back again

---
+++ tests/AcquirerTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace ERede\Acquiring;
+
+class AcquirerTest extends TestCase {
+
+  public function testFetchTransactionCredit() {
+
+    $acquirer = $this->getAcquirer();
+    $transaction = $acquirer->fetch(TransactionType::CREDIT);
+    $this->assertInstanceOf('\ERede\Acquiring\TransactionCredit', $transaction);
+
+  }
+
+  public function testFetchWrongTransaction() {
+
+    $acquirer = $this->getAcquirer();
+
+    try {
+      $transaction = $acquirer->fetch(8);
+    } catch(Error\WrongTransactionTypeException $e) {
+      $this->assertInstanceOf('\ERede\Acquiring\Error\WrongTransactionTypeException', $e);
+    }
+
+  }
+
+  public function testFetchWrongTransactionMessage() {
+
+    $acquirer = $this->getAcquirer();
+
+    try {
+      $transaction = $acquirer->fetch(8);
+    } catch(Error\WrongTransactionTypeException $e) {
+      $this->assertEquals("invalid transaction type", $e->getMessage());
+    }
+
+  }
+
+}
